b1f65cecd98851d4e18b92b917fd224627a8e827
dachir <dodziamouzou@gmail.com>
2024-03-27 11:47:26 +0000

    270320241247

--- mobilepos/mobile_pos.py
+++ mobilepos/mobile_pos.py
@@ -247,10 +247,10 @@ def get_documents(doctype=None,list_name=None,shop=None, limit=10, offset=0,name
             """
             SELECT (SELECT COUNT(*) + 1 FROM tabCustomer t2 WHERE t2.name <= t1.name) AS id,
             t1.name,CASE WHEN t1.name <> t1.customer_name THEN CONCAT(t1.name,' ',t1.customer_name) ELSE t1.customer_name END AS customer_name,
-            t1.customer_name as description,
+            t1.customer_name as description, t1.tax_id, t1.custom_other_buying_id
             t1.mobile_no as mobile,t1.email_id as email,t1.image, 0 as balance, t1.creation as created_at, t1.modified as updated_at,
             t1.territory, t1.warehouse, t1.company, t1.branch, t1.currency, t1.sales_person, t1.default_price_list as selling_price_list, t1.tax_id,
-            t1.custom_b2c
+            t1.custom_b2c, t1.tax_id, t1.custom_other_buying_id
             FROM (
                 SELECT c.*, s.warehouse, s.company, s.branch, s.currency, s.sales_person 
                 FROM tabShop s INNER JOIN `tabShop Territory` t ON t.parent = s.name INNER JOIN tabCustomer c ON t.territory = c.territory 
