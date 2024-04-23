7f8335ba6013d64b82024797ae8ab509859f99c4
dachir <dodziamouzou@gmail.com>
2024-04-23 20:20:39 +0000

    230420242120

--- mobilepos/mobilepos/doctype/shop_visit/shop_visit.py
+++ mobilepos/mobilepos/doctype/shop_visit/shop_visit.py
@@ -1,8 +1,12 @@
 # Copyright (c) 2024, Kossivi Amouzou and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class ShopVisit(Document):
-	pass
+	
+	def on_submit(self):
+		customer_doc = frappe.get_doc("Customer", self.customer)
+		customer_doc.last_visit = self.begin
+		customer_doc.save()
