3e0fdd3c6ea4debe24471020ba8ee58959b7dc7d
dachir <dodziamouzou@gmail.com>
2024-05-10 20:22:41 +0000

    100520242134

--- mobilepos/overrides/sales_invoice.py
+++ mobilepos/overrides/sales_invoice.py
@@ -8,7 +8,7 @@ class CustomSalesInvoice(SalesInvoice):
         frappe.msgprint(" customer:" + self.customer)
         frappe.msgprint( "name:" + self.name )
         frappe.msgprint( " grand_total:" + str(self.grand_total) )
-        frappe.msgprint(  " payment_type:" self.payment_type)
+        frappe.msgprint(  " payment_type:" + self.payment_type)
         if self.shop:
             if self.payment_type == "Cash":
                     visit =  frappe.db.sql(
@@ -19,6 +19,6 @@ class CustomSalesInvoice(SalesInvoice):
                         """, (self.name), as_dict = 1
                     )
                     if visit:
-                        frappe.msgprint(" visit:" visit[0].name)
+                        frappe.msgprint(" visit:" + visit[0].name)
                         create_pos_cash_invoice_payment(self.shop, self.company, self.customer, self.name, self.branch, self.grand_total, visit[0].name)
                         
