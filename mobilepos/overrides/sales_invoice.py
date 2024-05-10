3d3505bec94caf294aa3658a36cfa4e3d6053fd9
dachir <dodziamouzou@gmail.com>
2024-05-10 19:56:31 +0000

    100520242055

--- mobilepos/overrides/sales_invoice.py
+++ mobilepos/overrides/sales_invoice.py
@@ -3,7 +3,7 @@ from mobilepos.mobile_pos import create_pos_cash_invoice_payment
 
 class CustomSalesInvoice(SalesInvoice):
     def on_submit(self):
-        frappe.msgprint("shop: " + self.shop + " company:" + self.company + " customer:" + self.customer + " name:" + self.name + " grand_total:" + str(self.grand_total) + " payment_type:" self.payment_type)
+        #frappe.msgprint("shop: " + self.shop + " company:" + self.company + " customer:" + self.customer + " name:" + self.name + " grand_total:" + str(self.grand_total) + " payment_type:" self.payment_type)
         if self.shop:
             if self.payment_type == "Cash":
                     visit =  frappe.db.sql(
@@ -14,6 +14,6 @@ class CustomSalesInvoice(SalesInvoice):
                         """, (self.name), as_dict = 1
                     )
                     if visit:
-                        frappe.msgprint("shop: " + self.shop + " company:" + self.company + " customer:" + self.customer + " name:" + self.name + " grand_total:" + str(self.grand_total) + " visit:" visit[0].name)
+                        #frappe.msgprint("shop: " + self.shop + " company:" + self.company + " customer:" + self.customer + " name:" + self.name + " grand_total:" + str(self.grand_total) + " visit:" visit[0].name)
                         create_pos_cash_invoice_payment(self.shop, self.company, self.customer, self.name, self.branch, self.grand_total, visit[0].name)
                         
