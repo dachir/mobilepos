0d60a1d17e378ed0123898681b80cd365082bae4
dachir <dodziamouzou@gmail.com>
2024-05-14 17:59:00 +0000

    140520240919

--- mobilepos/overrides/sales_invoice.py
+++ mobilepos/overrides/sales_invoice.py
@@ -4,39 +4,7 @@ from mobilepos.mobile_pos import create_pos_cash_invoice_payment, process_cart_d
 from zatca2024.zatca2024.zatcasdkcode import zatca_Background_on_submit
 
 class CustomSalesInvoice(SalesInvoice):
-    def on_submit(self):
-        if self.shop:
-            items = frappe.db.sql(
-                """
-                SELECT item_code AS product_code, SUM(qty) AS quantity
-                FROM `tabSales Invoice Item`
-                WHERE parent = %s
-                """, (self.name), as_dict=1
-            )
-
-            invoice_details = process_cart_data(items, self.set_warehouse, self.branch, self.customer)
-            self.items.clear()
-            for i in invoice_details :
-                self.append('items', i)
-
-        #zatca_Background_on_submit(self)
-
-    def after_submit(self):
-        frappe.throw("test")
-        if self.shop:
-            if self.payment_type == "Cash":
-                    visit =  frappe.db.sql(
-                        """
-                        SELECT parent as name
-                        FROM `tabShop Visit Details` 
-                        WHERE document_name = %s
-                        """, (self.name), as_dict = 1
-                    )
-                    if visit:
-                        create_pos_cash_invoice_payment(self.shop, self.company, self.customer, self.name, self.branch, self.grand_total, visit[0].name)
-
-
     @frappe.whitelist()
     def recalculate_batch(self):
         frappe.throw("test " + self.name)
-                        
+        
