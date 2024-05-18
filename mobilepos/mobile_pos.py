fcb5fa4db964af107aae96c541720adb622bd7e1
dachir <dodziamouzou@gmail.com>
2024-05-18 08:27:22 +0000

    180520240921

--- mobilepos/mobile_pos.py
+++ mobilepos/mobile_pos.py
@@ -773,7 +773,7 @@ def create_invoice():
     pending = frappe.db.sql(
         """
         select sum(g.debit-g.credit) as amount from `tabGL Entry` g Inner Join `tabCustomer` c on c.name = g.party
-        where g.is_cancelled = 0 and c.territory = %s
+        where g.is_cancelled = 0 and c.territory = %s and c.custom_customer_account_type  IS NULL
         """, (shop.territory)
     )
 
