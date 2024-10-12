3fc6e5d4a89a1e5018e1c4380fe061e39d88a5fb
dachir <dodziamouzou@gmail.com>
2024-10-12 20:23:05 +0000

    121020242123

--- mobilepos/mobile_pos.py
+++ mobilepos/mobile_pos.py
@@ -1372,13 +1372,13 @@ def get_orders(customer):
 
     data = frappe.db.sql(
         """
-        SELECT ROW_NUMBER() OVER() AS id, i.item_code, SUM(i.qty - i.delivered_qty) AS qty, i.rate, SUM(i.amount) AS amount, i.is_free_item
-        FROM `tabSales Order` o INNER JOIN `tabSales Order Item` i ON i.parent = o.name
+        SELECT ROW_NUMBER() OVER() AS id, p.name AS product_id, i.item_code, SUM(i.qty - i.delivered_qty) AS qty, i.rate, SUM(i.amount) AS amount, i.is_free_item
+        FROM `tabSales Order` o INNER JOIN `tabSales Order Item` i ON i.parent = o.name INNER JOIN `tabShop Product` p ON p.product_code = i.item_code
         WHERE o.customer = %(customer)s AND o.docstatus = 1 AND i.delivered_qty < i.qty
-        GROUP BY i.item_code, i.qty, i.rate, i.is_free_item
+        GROUP BY p.name, i.item_code, i.qty, i.rate, i.is_free_item
         """, {"customer": customer}, as_dict=1
     )
-
+        #Union Commandes par le territory a qui appartient le liste de prix du client itinérant
     return frappe._dict({
         "total": len(data),
         "limit": 0,
