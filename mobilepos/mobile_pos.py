0228cd1a4483c6a46f878139042b5dbe255ea672
dachir <dodziamouzou@gmail.com>
2025-06-22 13:37:07 +0000

    220620251150

--- mobilepos/mobile_pos.py
+++ mobilepos/mobile_pos.py
@@ -584,7 +584,7 @@ def create_order(**request_dict):
             "doctype": "Sales Order Item",
             "item_code": i["item_code"],
             "qty": i["quantity"],   
-            "warehouse": request_dict.get("set_warehouse")         
+            #"warehouse": request_dict.get("set_warehouse")         
         })
         order_details.append(details)
 
@@ -599,7 +599,7 @@ def create_order(**request_dict):
         }
     )
 
-    frappe.throw(frappe.as_json(args))
+    #frappe.throw(frappe.as_json(args))
     try:
         sale = frappe.get_doc(args)
         #sale.ignore_pricing_rule = 1
