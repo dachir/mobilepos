5f9a36570d91810232648d08b1f30457044fc507
dachir <dodziamouzou@gmail.com>
2025-08-05 21:20:45 +0000

    040820251057

--- mobilepos/utils/sales_order.py
+++ mobilepos/utils/sales_order.py
@@ -8,6 +8,7 @@ def create_order(doc, method):
         branch = get_branch_name_by_location(add_doc.custom_latitude, add_doc.custom_longitude)
         if branch:
             doc.branch = branch
+            doc.territory = branch
             pl_name = frappe.db.get_value('Price List', { "custom_branch": branch }, 'name')
             if pl_name:
                 doc.selling_price_list =  pl_name
