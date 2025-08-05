2ae0c7379d2830756d4adfbc9308b397bad3eb09
dachir <dodziamouzou@gmail.com>
2025-08-05 21:18:24 +0000

    040820251057

--- mobilepos/utils/sales_order.py
+++ mobilepos/utils/sales_order.py
@@ -8,5 +8,10 @@ def create_order(doc, method):
         branch = get_branch_name_by_location(add_doc.custom_latitude, add_doc.custom_longitude)
         if branch:
             doc.branch = branch
+            pl_name = frappe.db.get_value('Price List', { "custom_branch": branch }, 'name')
+            if pl_name:
+                doc.selling_price_list =  pl_name
+            else:
+                frappe.throw("No Price List for this branch regarding this customer!")
         else:
             frappe.throw("We are not serving in this area!")
