4ad5ec48c5872cf662f276b580c4639a4586ce34
dachir <dodziamouzou@gmail.com>
2026-01-31 19:10:07 +0000

    310120261920

--- mobilepos/utils/sales_order.py
+++ mobilepos/utils/sales_order.py
@@ -5,7 +5,7 @@ from mobilepos.mobile_pos import get_branch_name_by_location
 def create_order(doc, method):
     if doc.customer.startswith("AC"):
         add_doc = frappe.get_doc("Address",doc.customer_address)
-        branch = get_branch_name_by_location(add_doc.custom_latitude, add_doc.custom_longitude)
+        branch = get_branch_name_by_location(add_doc.custom_latitude or doc.custom_latitude, add_doc.custom_longitude or doc.custom_longitude)
         if branch:
             doc.branch = branch
             doc.territory = branch
