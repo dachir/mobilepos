c471ffa8763d50356b294c6424723847fd0a3c1d
dachir <dodziamouzou@gmail.com>
2026-02-07 17:25:56 +0000

    070220261825

--- mobilepos/utils/sales_order.py
+++ mobilepos/utils/sales_order.py
@@ -1,10 +1,12 @@
 import frappe
 from mobilepos.mobile_pos import get_branch_name_by_location
+#from shapely.geometry import Point
 
 
 def create_order(doc, method):
     if doc.customer.startswith("AC"):
         add_doc = frappe.get_doc("Address",doc.customer_address)
+        #location = Point(add_doc.custom_longitude or doc.custom_longitude, add_doc.custom_latitude or doc.custom_latitude)
         branch = get_branch_name_by_location(add_doc.custom_longitude or doc.custom_latitude, add_doc.custom_latitude or doc.custom_longitude)
         if branch:
             doc.branch = branch
